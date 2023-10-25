e88d9f55a4f3027f8f72ff1aeb375819a35301f0
Sasha <oleksandrsy@gmail.com>
2023-10-25 15:58:40 +0300

    Created Role enum

---
+++ Agroforum.Domain/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agroforum.Domain
+{
+    public enum Role
+    {
+        User,
+        Entrepreneur
+    }
+}
