02d98f22a3e1909fd1830b97063929aa40241c8d
Sasha <oleksandrsy@gmail.com>
2023-10-25 15:59:06 +0300

    Created Account class

---
+++ Agroforum.Domain/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agroforum.Domain
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Role> Roles { get; set; }
+
+    }
+}
