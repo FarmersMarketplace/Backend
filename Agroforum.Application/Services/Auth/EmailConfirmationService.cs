b1c428b15562fe847f935e5db6e1be6bcd300848
Sasha <oleksandrsy@gmail.com>
2023-11-20 23:15:00 +0200

    Created FarmConfiguration class

--- Agroforum.Application/Services/Auth/EmailConfirmationService.cs
+++ Agroforum.Application/Services/Auth/EmailConfirmationService.cs
@@ -19,6 +19,66 @@ namespace Agroforum.Application.Services.Auth
         private SmtpClient SmtpClient { get; set; }
         private MailAddress FromAddress { get; set; }
         private string ConfirmationLink => "";
+        private string MessageBody = @"
+<!DOCTYPE html>
+<html lang=""en"">
+
+<head>
+   <meta charset=""UTF-8"">
+   <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"">
+   <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+   <title>Registration Confirmation</title>
+   <style>
+      body {
+         font-family: Arial, sans-serif;
+         background-color: #f4f4f4;
+         margin: 0;
+         padding: 0;
+      }
+
+      #container {
+         max-width: 600px;
+         margin: 20px auto;
+         background-color: #ffffff;
+         padding: 20px;
+         border-radius: 5px;
+         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      }
+
+      h2 {
+         color: #333;
+      }
+
+      p {
+         color: #555;
+      }
+
+      a {
+         color: #007BFF;
+      }
+   </style>
+</head>
+
+<body>
+   <div id=""container"">
+      <h2>Registration Confirmation</h2>
+      <p>Dear {0} {1},</p>
+      <p>Thank you for registering on our [website/service]. We are pleased to welcome you to our community!</p>
+      <p><strong>Name:</strong> {0} {1}<br>
+         <strong>Email:</strong> {2}<br>
+
+      </p>
+      <p>Please confirm your registration by clicking the link below:</p>
+      <p><a href=""http://localhost:5173/#/ConfirmEmail/{3}"">Confirm Registration</a></p>
+      <p>If you did not register on our [website/service], please ignore this email.</p>
+      <p>Thank you for choosing us! If you have any questions or issues, feel free to contact our support team at <a
+            href=""mailto:agroforum.support@gmail.com"">agroforum.support@gmail.com</a>.</p>
+      <p>Best regards,<br>
+         Agroforum</p>
+   </div>
+</body>
+
+</html>";
 
         public EmailConfirmationService()
         {
@@ -34,18 +94,17 @@ namespace Agroforum.Application.Services.Auth
         
         public async Task SendConfirmationEmail(string token, RegisterDto dto)
         {
-            var filePath = Directory.GetParent(Environment.CurrentDirectory) + "\\Agroforum.Application\\EmailTemplates\\verifyEmail.html";
-            string messageBody = File.ReadAllText(filePath);
-            messageBody = messageBody.Replace("{0}", dto.Name);
-            messageBody = messageBody.Replace("{1}", dto.Surname);
-            messageBody = messageBody.Replace("{2}", dto.Email);
-            messageBody = messageBody.Replace("{3}", token);
+            MessageBody = MessageBody.Replace("{0}", dto.Name);
+            MessageBody = MessageBody.Replace("{1}", dto.Surname);
+            MessageBody = MessageBody.Replace("{2}", dto.Email);
+            MessageBody = MessageBody.Replace("{3}", token);
+
 
             var mailMessage = new MailMessage
             {
                 From = FromAddress,
                 Subject = "Agroforum Registration Confirmation",
-                Body = messageBody,
+                Body = MessageBody,
                 IsBodyHtml = true
             };
             mailMessage.To.Add(dto.Email);
