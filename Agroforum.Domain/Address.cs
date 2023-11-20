c0b8b43fe3e11a2cddafcccbd432d3b6b23c762e
Sasha <oleksandrsy@gmail.com>
2023-11-20 23:14:28 +0200

    Created Address class

---
+++ Agroforum.Domain/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agroforum.Domain
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string Settlement { get; set; }
+        public string Street { get; set; } 
+        public string HouseNumber { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
