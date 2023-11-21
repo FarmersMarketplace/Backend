97b26ea65e93b74003958bbc22db501ebd4b5e2b
Sasha <oleksandrsy@gmail.com>
2023-11-21 16:36:17 +0200

    Created DeleteFarmDto

---
+++ Agroforum.Application/DataTransferObjects/Farm/DeleteFarmDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agroforum.Application.DataTransferObjects.Farm
+{
+    public class DeleteFarmDto
+    {
+        public Guid FarmId { get; set; }
+    }
+}
