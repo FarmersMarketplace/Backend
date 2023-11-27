5b75d259c70899f70319241f4c2708f50fd1c7c2
Sasha <oleksandrsy@gmail.com>
2023-11-27 20:25:18 +0200

    Created IEmployeeService interface

---
+++ Agroforum.Application/Services/Business/IEmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using Agroforum.Application.DataTransferObjects.Employee;
+using Agroforum.Application.ViewModels.Employee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agroforum.Application.Services.Business
+{
+    public interface IEmployeeService
+    {
+        Task Add(AddPermissionsDto addPermissionsDto);
+        Task Update(UpdatePermissionsDto updatePermissionsDto);
+        Task Delete(Guid permissionsId);
+        Task<PermissionsVm> Get(Guid permissionsId);
+        Task<EmployeeListVm> GetAll(Guid farmId);
+        Task<EmployeeListVm> Search(string identifier);
+        Task Confirm(Guid permissionsId, Guid accountId);
+    }
+}
