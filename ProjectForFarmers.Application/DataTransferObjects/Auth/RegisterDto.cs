8d41d102c18c407269d5527a420e92969ec6d25d
Sasha <oleksandrsy@gmail.com>
2024-01-24 18:13:42 +0200

    Changed IMapWith to profiles

--- ProjectForFarmers.Application/DataTransferObjects/Auth/RegisterDto.cs
+++ ProjectForFarmers.Application/DataTransferObjects/Auth/RegisterDto.cs
@@ -4,7 +4,7 @@ using ProjectForFarmers.Domain;
 
 namespace ProjectForFarmers.Application.DataTransferObjects.Auth
 {
-    public class RegisterDto : IMapWith<Domain.Account>
+    public class RegisterDto
     {
         public string Name { get; set; }
         public string Surname { get; set; }
@@ -13,15 +13,6 @@ namespace ProjectForFarmers.Application.DataTransferObjects.Auth
         public string ConfirmPassword{ get; set; }
         public Role Role { get; set; }
 
-        public void Mapping(Profile profile)
-        {
-            profile.CreateMap<RegisterDto, Domain.Account>()
-                .ForMember(account => account.Id, opt => opt.MapFrom(dto => Guid.NewGuid()))
-                .ForMember(account => account.Name, opt => opt.MapFrom(dto => dto.Name))
-                .ForMember(account => account.Surname, opt => opt.MapFrom(dto => dto.Surname))
-                .ForMember(account => account.Email, opt => opt.MapFrom(dto => dto.Email))
-                .ForMember(account => account.Password, opt => opt.MapFrom(dto => dto.Password))
-                .ForMember(account => account.Role, opt => opt.MapFrom(dto => dto.Role));
-        }
+        
     }
 }
