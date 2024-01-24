e2cff32b44ac62752016905bc30455cf6b7bb154
Sasha <oleksandrsy@gmail.com>
2024-01-24 20:32:06 +0200

    Created Order class

---
+++ ProjectForFarmers.Domain/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForFarmers.Domain
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public string Number { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerEmail { get; set; }
+        public decimal PaymentTotal { get; set; }
+        public PaymentType PaymentType { get; set; }
+
+    }
+
+}
