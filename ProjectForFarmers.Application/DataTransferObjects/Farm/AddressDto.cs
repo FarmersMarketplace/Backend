0ffacd1ec95028d48cf83c10aeae7bbebc2464f0
Sasha <oleksandrsy@gmail.com>
2024-01-24 17:57:25 +0200

    Implemented IMapWith in AddressDto

--- ProjectForFarmers.Application/DataTransferObjects/Farm/AddressDto.cs
+++ ProjectForFarmers.Application/DataTransferObjects/Farm/AddressDto.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using AutoMapper;
+using ProjectForFarmers.Application.DataTransferObjects.Auth;
+using ProjectForFarmers.Application.Interfaces;
+using ProjectForFarmers.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectForFarmers.Application.DataTransferObjects.Farm
 {
-    public class AddressDto
+    public class AddressDto : IMapWith<Address>
     {
         public string Region { get; set; }
         public string District { get; set; }
@@ -15,6 +20,19 @@ namespace ProjectForFarmers.Application.DataTransferObjects.Farm
         public string HouseNumber { get; set; }
         public string PostalCode { get; set; }
         public string Note { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<AddressDto, Address>()
+                .ForMember(address => address.Id, opt => opt.MapFrom(dto => Guid.NewGuid()))
+                .ForMember(address => address.Region, opt => opt.MapFrom(dto => dto.Region))
+                .ForMember(address => address.District, opt => opt.MapFrom(dto => dto.District))
+                .ForMember(address => address.Settlement, opt => opt.MapFrom(dto => dto.Settlement))
+                .ForMember(address => address.Street, opt => opt.MapFrom(dto => dto.Street))
+                .ForMember(address => address.HouseNumber, opt => opt.MapFrom(dto => dto.HouseNumber))
+                .ForMember(address => address.PostalCode, opt => opt.MapFrom(dto => dto.PostalCode))
+                .ForMember(address => address.Note, opt => opt.MapFrom(dto => dto.Note));
+        }
     }
 
 }
