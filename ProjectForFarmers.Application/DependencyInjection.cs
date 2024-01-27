f7e670b79e7fa5d24201a8f9fedddbe7c6307d0b
Sasha <oleksandrsy@gmail.com>
2024-01-27 15:16:49 +0200

    Added Profiles to application

--- ProjectForFarmers.Application/DependencyInjection.cs
+++ ProjectForFarmers.Application/DependencyInjection.cs
@@ -8,15 +8,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectForFarmers.Application.Mappings;
+using AutoMapper;
 
 namespace ProjectForFarmers.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
+        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddTransient<IAuthService, AuthService>();
             services.AddTransient<IFarmService, FarmService>();
+
+            services.AddAutoMapper(config =>
+            {
+                config.AddProfile(new AuthMappingProfile());
+                config.AddProfile(new FarmMappingProfile(configuration));
+            });
+
             return services;
         }
     }
