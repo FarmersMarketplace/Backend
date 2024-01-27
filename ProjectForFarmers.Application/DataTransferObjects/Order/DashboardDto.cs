453d6062ec83d557abf113a2472dfc0b28756d94
Sasha <oleksandrsy@gmail.com>
2024-01-27 15:16:26 +0200

    Created DashboardDto class

---
+++ ProjectForFarmers.Application/DataTransferObjects/Order/DashboardDto.cs
@@ -0,0 +1,18 @@
+﻿using ProjectForFarmers.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForFarmers.Application.DataTransferObjects.Order
+{
+    public class DashboardDto
+    {
+        public Guid ProducerId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Producer Producer { get; set; }
+    }
+
+}
