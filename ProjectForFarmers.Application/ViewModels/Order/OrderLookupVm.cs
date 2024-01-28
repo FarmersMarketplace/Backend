655933663a3ec6835e342d6772aea24b5c6c1922
Sasha <oleksandrsy@gmail.com>
2024-01-28 17:57:17 +0200

    Created OrderLookupVm class

---
+++ ProjectForFarmers.Application/ViewModels/Order/OrderLookupVm.cs
@@ -0,0 +1,23 @@
+﻿using ProjectForFarmers.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForFarmers.Application.ViewModels.Order
+{
+    public class OrderLookupVm
+    {
+        public Guid Id { get; set; }
+        public uint Number { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerEmail { get; set; }
+        public decimal TotalPayment { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+
+}
