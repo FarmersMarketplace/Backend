29727e7ae4ae6f0d51e8a8e111686d73e8788581
Sasha <oleksandrsy@gmail.com>
2024-02-01 20:06:01 +0200

    Added Phone to Account

--- ProjectForFarmers.Domain/Account.cs
+++ ProjectForFarmers.Domain/Account.cs
@@ -13,6 +13,7 @@ namespace ProjectForFarmers.Domain
         public string Name { get; set; }
         public string Surname { get; set; }
         public string? Email { get; set; }
+        public string? Phone { get; set; }
         public string Password { get; set; }
         public Role Role { get; set; }
 
