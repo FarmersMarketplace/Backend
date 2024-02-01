1d660c38ce2eda9707dcb3fbcc6d77692c89f917
Sasha <oleksandrsy@gmail.com>
2024-02-01 21:35:16 +0200

    Created Product

--- ProjectForFarmers.Domain/Product.cs
+++ ProjectForFarmers.Domain/Product.cs
@@ -15,6 +15,22 @@ namespace ProjectForFarmers.Domain
         public Guid SubcategoryId { get; set; }
         public Category Category { get; set; }
         public Subcategory Subcategory { get; set; }
+        public ProductStatus Status { get; set; }
+        public Producer Producer { get; set; }
+        public Guid ProducerId { get; set; }
+        public string PackagingType { get; set; }
+        public string UnitOfMeasurement { get; set; }
+        public string PricePerOne { get; set; }
+        public int MinPurchaseQuantity { get; set; }
+        public int Count { get; set; }
+        public List<PaymentType> PaymentTypes { get; set; }
+        public List<ReceivingType> ReceivingTypes { get; set; }
+        public Guid SupplyPointId { get; set; }
+        public Guid? DeliveryPointId { get; set; }
+        public Address SupplyPoint { get; set; }
+        public Address? DeliveryPoint { get; set; }
+        public List<string> ImagesNames { get; set; }
+        public List<Characteristic> Characteristics { get; set; }
     }
 
 }
