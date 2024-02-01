2e0a8e783f9af0d29a7a8a5ce5318b94a12559ae
Sasha <oleksandrsy@gmail.com>
2024-02-01 20:05:40 +0200

    Created OrderVm

---
+++ ProjectForFarmers.Application/ViewModels/Order/OrderVm.cs
@@ -0,0 +1,29 @@
+﻿using ProjectForFarmers.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForFarmers.Application.ViewModels.Order
+{
+    public class OrderVm
+    {
+        public Guid Id { get; set; }
+        public string Number { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ReceiveDate { get; set; }
+        public decimal TotalPayment { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public ReceivingType ReceivingType { get; set; }
+        public AddressVm SupplyPoint { get; set; }
+        public AddressVm? DeliveryPoint { get; set; }
+        public OrderStatus Status { get; set; }
+        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPhone { get; set; }
+        public List<OrderItemVm> Items { get; set; }
+    }
+
+}
--- ProjectForFarmers.Domain/Order.cs
+++ ProjectForFarmers.Domain/Order.cs
@@ -11,14 +11,15 @@
         public PaymentStatus PaymentStatus { get; set; }
         public ReceivingType ReceivingType { get; set; }
         public Guid SupplyPointId { get; set; }
-        public Guid DeliveryPointId { get; set; }
+        public Guid? DeliveryPointId { get; set; }
         public Address SupplyPoint { get; set; }
-        public Address DeliveryPoint { get; set; }
+        public Address? DeliveryPoint { get; set; }
         public Producer Producer { get; set; }
         public OrderStatus Status { get; set; }
         public Guid ProducerId { get; set; }
         public Guid CustomerId { get; set; }
         public virtual Account Customer { get; set; }
+        public List<OrderItem> Items { get; set; }
     }
 
 }
