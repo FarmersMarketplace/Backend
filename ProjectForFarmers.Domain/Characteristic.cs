88e9253874ef743d6d43869a0d213e6d0865ccd0
Sasha <oleksandrsy@gmail.com>
2024-02-01 20:30:30 +0200

    Created Characteristic

---
+++ ProjectForFarmers.Domain/Characteristic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForFarmers.Domain
+{
+    public class Characteristic
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+}
