4801c1884521812cb3c3ee8fbb9d33b93df06153
Sasha <oleksandrsy@gmail.com>
2024-02-01 20:04:48 +0200

    Created OrderItem

---
+++ ProjectForFarmers.Domain/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForFarmers.Domain
+{
+    public class OrderItem
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public Guid OrderId { get; set; }
+        public uint Count { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+
+}
