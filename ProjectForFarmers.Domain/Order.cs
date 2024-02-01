b38f611e5b75b687f8f76bb259dd6ef6df427b9f
Sasha <oleksandrsy@gmail.com>
2024-02-01 14:04:14 +0200

    Added properties to Order

--- ProjectForFarmers.Domain/Order.cs
+++ ProjectForFarmers.Domain/Order.cs
@@ -1,26 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjectForFarmers.Domain
+﻿namespace ProjectForFarmers.Domain
 {
     public class Order
     {
         public Guid Id { get; set; }
         public uint Number { get; set; }
         public DateTime CreationDate { get; set; }
-        public string CustomerName { get; set; }
-        public string CustomerPhone { get; set; }
-        public string CustomerEmail { get; set; }
+        public DateTime ReceiveDate { get; set; }
         public decimal TotalPayment { get; set; }
         public PaymentType PaymentType { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public ReceivingType ReceivingType { get; set; }
+        public Guid SupplyPointId { get; set; }
+        public Guid DeliveryPointId { get; set; }
+        public Address SupplyPoint { get; set; }
+        public Address DeliveryPoint { get; set; }
         public Producer Producer { get; set; }
         public OrderStatus Status { get; set; }
         public Guid ProducerId { get; set; }
-        public virtual Account Customer { get; set; }
         public Guid CustomerId { get; set; }
+        public virtual Account Customer { get; set; }
     }
 
 }
