ef02c7e6ae036106be94b679e9cf25ae2667f5f9
Sasha <oleksandrsy@gmail.com>
2024-02-01 20:28:28 +0200

    Created CategoryConfiguration

---
+++ ProjectForFarmers.Persistence/EntityTypeConfigurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using ProjectForFarmers.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForFarmers.Persistence.EntityTypeConfigurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(category => category.Id);
+            builder.Property(category => category.Name).IsRequired();
+
+            builder.HasIndex(category => category.Id).IsUnique();
+        }
+    }
+
+}
