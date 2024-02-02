5b2bb565175c2e5fc2ae4951c1e4f26fd0652c46
Sasha <oleksandrsy@gmail.com>
2024-02-02 18:28:53 +0200

    Created CategoryListVm

---
+++ ProjectForFarmers.Application/ViewModels/Category/CategoryListVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForFarmers.Application.ViewModels.Category
+{
+    public class CategoryListVm
+    {
+        public List<Domain.Category> Categories { get; set; }
+    }
+
+}
