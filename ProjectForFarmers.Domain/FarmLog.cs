196be8b428c035dc3f6de9fe3628b3a36025372d
Sasha <oleksandrsy@gmail.com>
2024-02-07 18:20:52 +0200

    Created FarmLog

---
+++ ProjectForFarmers.Domain/FarmLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForFarmers.Domain
+{
+    public class FarmLog
+    {
+        public Guid Id { get; set; }
+        public Guid FarmId { get; set; }
+        public string Message { get; set; }
+        public List<string> Parameters { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+
+}
