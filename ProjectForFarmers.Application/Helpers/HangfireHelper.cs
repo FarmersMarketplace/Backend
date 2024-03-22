e784c3db35a36116ce2fcec99549cce3ef568e97
Sasha <oleksandrsy@gmail.com>
2024-03-22 17:40:51 +0200

    Fixed LastDayOfCurrentMonth and LastDayOfNextMonth in HangfireHelper

--- ProjectForFarmers.Application/Helpers/HangfireHelper.cs
+++ ProjectForFarmers.Application/Helpers/HangfireHelper.cs
@@ -8,7 +8,7 @@ namespace FarmersMarketplace.Application.Helpers
 {
     public static class HangfireHelper
     {
-        public static DateTime LastDayOfCurrentMonth => new DateTime(DateTimeOffset.UtcNow.Year,
+        public static DateTime LastDayOfCurrentMonth => new DateTime(DateTime.UtcNow.Year,
                 DateTime.UtcNow.Month,
                 1, 0, 0, 0)
                     .AddMonths(1)
@@ -22,15 +22,22 @@ namespace FarmersMarketplace.Application.Helpers
 
         public static async Task UpdateStatistics()
         {
-            Log.Information("1");
             BackgroundJob.Schedule(() => UpdateStatistics(), LastDayOfNextMonth());
 
             await StatisticService.UpdateAllStatistics(LastDayOfCurrentMonth);
         }
 
-        public static DateTimeOffset LastDayOfNextMonth()
+        public static DateTime LastDayOfNextMonth()
         {
-            return LastDayOfCurrentMonth.AddMonths(1);
+            int year = LastDayOfCurrentMonth.Year;
+            int month = LastDayOfCurrentMonth.Month;
+
+            int nextMonth = month == 12 ? 1 : month + 1;
+            int nextYear = month == 12 ? year + 1 : year;
+
+            DateTime lastDayOfNextMonth = new DateTime(nextYear, nextMonth, 1).AddMonths(1).AddDays(-1);
+
+            return lastDayOfNextMonth;
         }
 
         public static void RegisterTasks(IServiceProvider services)
