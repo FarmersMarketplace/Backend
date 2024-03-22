28b72b505d47267890c4f0d18e13405d2c39a83b
Sasha <oleksandrsy@gmail.com>
2024-03-22 22:10:15 +0200

    Created Seller

---
+++ ProjectForFarmers.Domain/Seller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmersMarketplace.Domain
+{
+    public class Seller : Account
+    {
+        public List<string>? ImagesNames { get; set; }
+        public Guid PaymentDataId { get; set; }
+        public ProducerPaymentData? PaymentData { get; set; }
+        public Guid AddressId { get; set; }
+        public Address? Address { get; set; }
+        public Guid ScheduleId { get; set; }
+        public virtual Schedule Schedule { get; set; }
+        public string? FirstSocialPageUrl { get; set; }
+        public string? SecondSocialPageUrl { get; set; }
+        public List<ReceivingMethod>? ReceivingMethods { get; set; }
+        public List<PaymentType>? PaymentTypes { get; set; }
+        public List<Guid>? Categories { get; set; }
+        public List<Guid>? Subcategories { get; set; }
+
+    }
+
+}
