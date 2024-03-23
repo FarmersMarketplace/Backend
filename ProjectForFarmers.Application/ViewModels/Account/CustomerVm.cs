76c04dff741466eb46fe79447ac79ee958a430d5
Sasha <oleksandrsy@gmail.com>
2024-03-23 12:57:00 +0200

    Created CustomerVm

---
+++ ProjectForFarmers.Application/ViewModels/Account/CustomerVm.cs
@@ -0,0 +1,25 @@
+﻿using FarmersMarketplace.Application.DataTransferObjects.Account;
+using FarmersMarketplace.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmersMarketplace.Application.ViewModels.Account
+{
+    public class CustomerVm
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Gender? Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string AvatarName { get; set; }
+        public CustomerPaymentDataVm PaymentData { get; set; }
+        public CustomerAddressVm Address { get; set; }
+    }
+
+}
