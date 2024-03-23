3b2c768ccd5ec71e1979c19a0582d79c245d2f77
Sasha <oleksandrsy@gmail.com>
2024-03-23 13:12:18 +0200

    Created FarmerVm

---
+++ ProjectForFarmers.Application/ViewModels/Account/FarmerVm.cs
@@ -0,0 +1,24 @@
+﻿using FarmersMarketplace.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmersMarketplace.Application.ViewModels.Account
+{
+    public class FarmerVm
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? Patronymic { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public Gender? Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? AvatarName { get; set; }
+        public ProducerPaymentDataVm? PaymentData { get; set; }
+        public AddressVm? Address { get; set; }
+    }
+
+}
