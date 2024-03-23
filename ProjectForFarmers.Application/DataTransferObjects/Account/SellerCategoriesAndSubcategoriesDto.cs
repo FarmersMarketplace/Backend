5a7f7f128cf62ed7abbe3dc7dbb7fb58b234ee3f
Sasha <oleksandrsy@gmail.com>
2024-03-23 13:12:27 +0200

    Created SellerCategoriesAndSubcategoriesDto

---
+++ ProjectForFarmers.Application/DataTransferObjects/Account/SellerCategoriesAndSubcategoriesDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmersMarketplace.Application.DataTransferObjects.Account
+{
+    public class SellerCategoriesAndSubcategoriesDto
+    {
+        public List<Guid> Categories { get; set; }
+        public List<Guid> Subcategories { get; set; }
+    }
+
+}
