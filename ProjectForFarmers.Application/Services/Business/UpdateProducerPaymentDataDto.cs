5d56780f913d0d39acc9b6523777866f9e2037e6
Sasha <oleksandrsy@gmail.com>
2024-03-23 13:08:26 +0200

    Created

---
+++ ProjectForFarmers.Application/Services/Business/UpdateProducerPaymentDataDto.cs
@@ -0,0 +1,12 @@
+﻿using FarmersMarketplace.Application.DataTransferObjects;
+using FarmersMarketplace.Domain;
+
+namespace FarmersMarketplace.Application.Services.Business
+{
+    public class UpdateProducerPaymentDataDto
+    {
+        public Producer Producer { get; set; }
+        public bool HasOnlinePayment { get; set; }
+        public ProducerPaymentDataDto PaymentDataDto { get; set; }
+    }
+}
