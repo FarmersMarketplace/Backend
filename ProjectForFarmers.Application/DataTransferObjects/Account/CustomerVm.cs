5f908fc37ff701281f1b03882b24b59d6339257f
Sasha <oleksandrsy@gmail.com>
2024-03-23 12:35:24 +0200

    Created CustomerPaymentDataDto

---
+++ ProjectForFarmers.Application/DataTransferObjects/Account/CustomerVm.cs
@@ -0,0 +1,25 @@
+﻿using FarmersMarketplace.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmersMarketplace.Application.DataTransferObjects.Account
+{
+    public class CustomerVm
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Gender? Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string AvatarName { get; set; }
+        public CustomerPaymentDataDto PaymentData { get; set; }
+        public Guid AddressId { get; set; }
+        public CustomerAddress? Address { get; set; }
+    }
+
+}
