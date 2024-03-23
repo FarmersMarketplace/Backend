952e878eb791bd85d27545ce5bffa58e75a1fa6b
Sasha <oleksandrsy@gmail.com>
2024-03-23 12:37:44 +0200

    Created CustomerAddressDto

---
+++ ProjectForFarmers.Application/DataTransferObjects/Account/CustomerAddressDto.cs
@@ -0,0 +1,15 @@
+﻿using FarmersMarketplace.Application.DataTransferObjects.Farm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmersMarketplace.Application.DataTransferObjects.Account
+{
+    public class CustomerAddressDto : AddressDto
+    {
+        public string Apartment { get; set; }
+    }
+
+}
