b5b6d25ab4014a9d3765fa925e99888c0133d603
Sasha <oleksandrsy@gmail.com>
2024-03-29 15:54:50 +0200

    Created Feedback, ProductFeedback and ProducerFeedback classes

---
+++ ProjectForFarmers.Domain/Feedbacks/ProducerFeedback.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmersMarketplace.Domain.Feedbacks
+{
+    public class ProducerFeedback : Feedback
+    {
+        public Producer Producer { get; set; }
+        public Guid ProducerId { get; set; }
+    }
+
+}
---
+++ ProjectForFarmers.Domain/Feedbacks/ProductFeedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmersMarketplace.Domain.Feedbacks
+{
+    public class ProductFeedback : Feedback
+    {
+        public Guid ProductId { get; set; }
+    }
+
+}
