2aae21366c23b1c8cb6f288f5090503fb90f0aed
Sasha <oleksandrsy@gmail.com>
2024-03-30 22:56:16 +0200

    Changed ISearchProvider to more jeneral view

--- ProjectForFarmers.Application/Interfaces/ISearchProvider.cs
+++ ProjectForFarmers.Application/Interfaces/ISearchProvider.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FarmersMarketplace.Application.DataTransferObjects.Product;
+using FarmersMarketplace.Application.ViewModels.Product;
 
 namespace FarmersMarketplace.Application.Interfaces
 {
-    public interface ISearchProvider
+    public interface ISearchProvider<TRequest, TRestonse>
     {
-        Task<CustomerListVm> GetProducts();
+        Task<TRestonse> Search(TRequest request);
     }
 }
