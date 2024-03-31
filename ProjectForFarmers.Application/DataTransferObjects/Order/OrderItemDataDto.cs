dd3a5b59285f74f79132e8c4c2f72e4a21092196
Sasha <oleksandrsy@gmail.com>
2024-03-31 14:36:50 +0300

    Created OrderItemDataDto

---
+++ ProjectForFarmers.Application/DataTransferObjects/Order/OrderItemDataDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FarmersMarketplace.Application.DataTransferObjects.Order
+{
+    public class OrderItemDataDto
+    {
+        public Guid ProductId { get; set; }
+        public uint Count { get; set; }
+    }
+
+}
