5d9d576558f57d776b1601215f7bbab0acbaa3c2
Sasha <oleksandrsy@gmail.com>
2024-04-03 14:00:44 +0300

    Added Autocomplete to ISearchProvider

--- ProjectForFarmers.Application/Interfaces/ISearchProvider.cs
+++ ProjectForFarmers.Application/Interfaces/ISearchProvider.cs
@@ -1,7 +1,8 @@
 ﻿namespace FarmersMarketplace.Application.Interfaces
 {
-    public interface ISearchProvider<TRequest, TRestonse>
+    public interface ISearchProvider<TRequest, TAutocompleteRequest, TRestonse>
     {
         Task<TRestonse> Search(TRequest request);
+        Task<List<string>> Autocomplete(TAutocompleteRequest request);
     }
 }
