1541be6001d93ee3d99c8949efed3db8a2e500a2
Sasha <oleksandrsy@gmail.com>
2024-06-10 20:15:30 +0300

    Created CustomerOrderListVm

---
+++ ProjectForFarmers.Application/ViewModels/Order/CustomerOrderListVm.cs
@@ -0,0 +1,8 @@
+﻿namespace FarmersMarketplace.Application.ViewModels.Order
+{
+    public class CustomerOrderListVm
+    {
+        public List<CustomerOrderLookupVm> Orders { get; set; }
+        public int Count { get; set; }
+    }
+}
