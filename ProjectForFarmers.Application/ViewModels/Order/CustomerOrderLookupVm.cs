f0c2948e1dec1d7d5e6356bbf5df55929f3798d7
Sasha <oleksandrsy@gmail.com>
2024-06-10 20:15:10 +0300

    Created GetCustomerOrderListDto

---
+++ ProjectForFarmers.Application/ViewModels/Order/CustomerOrderLookupVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmersMarketplace.Application.ViewModels.Order
+{
+    public class CustomerOrderLookupVm
+    {
+        public Guid Id { get; set; }
+        public string Number { get; set; }
+    }
+}
