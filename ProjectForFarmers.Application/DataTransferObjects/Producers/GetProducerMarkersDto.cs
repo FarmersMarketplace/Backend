0b2463c5c8e75e06c63ca7e55e262959c94a3428
Sasha <oleksandrsy@gmail.com>
2024-06-11 17:52:35 +0300

    Created GetProducerMarkersDto

---
+++ ProjectForFarmers.Application/DataTransferObjects/Producers/GetProducerMarkersDto.cs
@@ -0,0 +1,13 @@
+﻿using FarmersMarketplace.Application.Filters;
+
+namespace FarmersMarketplace.Application.DataTransferObjects.Producers
+{
+    public class GetProducerMarkersDto
+    {
+        public double MaxLatitude { get; set; }
+        public double MaxLongitude { get; set; }
+        public double MinLatitude { get; set; }
+        public double MinLongitude { get; set; }
+        public ProducerFilter? Filter { get; set; }
+    }
+}
