f71c29836ae7daabcdd349fe7f506023987a7c80
Sasha <oleksandrsy@gmail.com>
2024-06-11 12:23:18 +0300

    Created ProducerFilter

---
+++ ProjectForFarmers.Application/Filters/ProducerFilter.cs
@@ -0,0 +1,13 @@
+﻿using FarmersMarketplace.Domain;
+
+namespace FarmersMarketplace.Application.Filters
+{
+    public class ProducerFilter
+    {
+        public Producer? Producer { get; set; }
+        public HashSet<Guid>? Farms { get; set; }
+        public HashSet<Guid>? Sellers { get; set; }
+        public string? Region { get; set; }
+        public HashSet<Guid>? Subcategories { get; set; }
+    }
+}
