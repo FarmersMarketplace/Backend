78ec797775d9e082ede36eb5a5bcd6e833a7db5c
Sasha <oleksandrsy@gmail.com>
2024-06-11 07:59:57 +0300

    Created ICacheSynchronizer

---
+++ ProjectForFarmers.Application/Interfaces/ICacheSynchronizer.cs
@@ -0,0 +1,9 @@
+﻿namespace FarmersMarketplace.Application.Interfaces
+{
+    public interface ICacheSynchronizer<T>
+    {
+        Task Create(T obj);
+        Task Update(T obj);
+        Task Delete(Guid id);
+    }
+}
