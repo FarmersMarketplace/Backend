74e9e078ba7c9d9792b8ec69e646493621d21cd3
Sasha <oleksandrsy@gmail.com>
2024-06-11 12:23:36 +0300

    Created GetProducerListDto

---
+++ ProjectForFarmers.Application/DataTransferObjects/Producers/GetProducerListDto.cs
@@ -0,0 +1,12 @@
+﻿using FarmersMarketplace.Application.Filters;
+
+namespace FarmersMarketplace.Application.DataTransferObjects.Producers
+{
+    public class GetProducerListDto
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public string? Query { get; set; }
+        public ProducerFilter? Filter { get; set; }
+    }
+}
