96cbbf2ab5e4b69111393d3cd994207b8be778ab
Sasha <oleksandrsy@gmail.com>
2024-06-11 12:23:29 +0300

    Created ProducerAutocompleteDto

---
+++ ProjectForFarmers.Application/DataTransferObjects/Producers/ProducerAutocompleteDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FarmersMarketplace.Application.DataTransferObjects.Producers
+{
+    public class ProducerAutocompleteDto
+    {
+        public int Count { get; set; }
+        public string Query { get; set; }
+    }
+}
