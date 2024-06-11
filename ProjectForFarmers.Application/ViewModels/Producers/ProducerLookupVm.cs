84c1394a32aea1594456700501f0dde9d78900dc
Sasha <oleksandrsy@gmail.com>
2024-06-11 11:37:49 +0300

    Created ProducerLookupVm

---
+++ ProjectForFarmers.Application/ViewModels/Producers/ProducerLookupVm.cs
@@ -0,0 +1,14 @@
+﻿using FarmersMarketplace.Domain;
+
+namespace FarmersMarketplace.Application.ViewModels.Producers
+{
+    public class ProducerLookupVm
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Producer Producer { get; set; }
+        public string ImageName { get; set; }
+        public uint FeedbacksCount { get; set; }
+        public float Rating { get; set; }
+    }
+}
