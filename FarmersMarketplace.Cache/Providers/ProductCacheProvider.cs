d2d061ae7787815e3ce496007a838f5dd0a122d1
Sasha <oleksandrsy@gmail.com>
2024-06-13 12:51:30 +0300

    Created ProductCacheProvider

---
+++ FarmersMarketplace.Cache/Providers/ProductCacheProvider.cs
@@ -0,0 +1,22 @@
+﻿using FarmersMarketplace.Domain;
+using StackExchange.Redis;
+
+namespace FarmersMarketplace.Cache.Providers
+{
+    public class ProductCacheProvider : CacheProvider<Product>
+    {
+        public ProductCacheProvider(IConnectionMultiplexer redis) : base(redis)
+        {
+        }
+
+        protected override Guid GetId(Product obj)
+        {
+            return obj.Id;
+        }
+
+        protected override string Key(Guid id)
+        {
+            return $"product-{id}";
+        }
+    }
+}
