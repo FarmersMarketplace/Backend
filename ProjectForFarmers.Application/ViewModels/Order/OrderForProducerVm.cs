3d50bdc63d231ba0a2c1a0e78d482d30d38b6cdc
Sasha <oleksandrsy@gmail.com>
2024-06-15 13:25:16 +0300

    Created ProductTrendService

---
+++ ProjectForFarmers.Application/ViewModels/Order/OrderForProducerVm.cs
@@ -0,0 +1,28 @@
+﻿using FarmersMarketplace.Application.ViewModels.Account;
+using FarmersMarketplace.Domain;
+using FarmersMarketplace.Domain.Orders;
+using FarmersMarketplace.Domain.Payment;
+
+namespace FarmersMarketplace.Application.ViewModels.Order
+{
+    public class OrderForProducerVm
+    {
+        public Guid Id { get; set; }
+        public string Number { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ReceiveDate { get; set; }
+        public decimal TotalPayment { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public ReceivingMethod ReceivingMethod { get; set; }
+        public CustomerAddressVm? DeliveryPoint { get; set; }
+        public OrderStatus Status { get; set; }
+        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerAdditionalPhone { get; set; }
+        public List<OrderItemVm> Items { get; set; }
+    }
+
+}
