333b811dc2971bbd921dbb3dc85116a9eb30c605
Sasha <oleksandrsy@gmail.com>
2024-06-15 15:39:00 +0300

    Created FeedbackSearchProfile

---
+++ FarmersMarketplace.Elasticsearch/Mappings/FeedbackSearchProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FarmersMarketplace.Domain.Feedbacks;
+using FarmersMarketplace.Elasticsearch.Documents;
+
+namespace FarmersMarketplace.Elasticsearch.Mappings
+{
+    public class FeedbackSearchProfile : Profile
+    {
+        public FeedbackSearchProfile()
+        {
+            MapFeedbackToFeedbackDocument();
+        }
+
+        private void MapFeedbackToFeedbackDocument()
+        {
+            CreateMap<Feedback, FeedbackDocument>()
+                .ForMember(document => document.CustomerId, opt => opt.MapFrom(feedback => feedback.CustomerId))
+                .ForMember(document => document.CustomerName, opt => opt.MapFrom(feedback => feedback.Customer.Name + " " + feedback.Customer.Surname))
+                .ForMember(document => document.CustomerId, opt => opt.MapFrom(feedback => feedback.Customer.AvatarName));
+        }
+    }
+}
