285ade67b3c20197eeede8667710233cf6d6f91a
Sasha <oleksandrsy@gmail.com>
2024-06-15 14:53:07 +0300

    Created FeedbackDocument

---
+++ FarmersMarketplace.Elasticsearch/Documents/FeedbackDocument.cs
@@ -0,0 +1,15 @@
+﻿using FarmersMarketplace.Domain.Accounts;
+
+namespace FarmersMarketplace.Elasticsearch.Documents
+{
+    public class FeedbackDocument
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public string Comment { get; set; }
+        public float Rating { get; set; }
+        public DateTime Date { get; set; }
+        public Guid ReviewedEntityId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
