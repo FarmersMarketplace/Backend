187ad6cf81e183897f320c04c0a8644b669ad9fd
Sasha <oleksandrsy@gmail.com>
2024-06-16 17:06:27 +0300

    Created ProductFeedbackCollectionConfiguration

---
+++ ProjectForFarmers.Persistence/EntityTypeConfigurations/ProductFeedbackCollectionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using FarmersMarketplace.Domain.Feedbacks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FarmersMarketplace.Persistence.EntityTypeConfigurations
+{
+    public class ProductFeedbackCollectionConfiguration : IEntityTypeConfiguration<ProductFeedbackCollection>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeedbackCollection> builder)
+        {
+            builder.ToTable("ProductFeedbackCollections");
+
+            builder.HasKey(collection => collection.Id);
+            builder.HasIndex(collection => collection.Id).IsUnique();
+        }
+    }
+}
